388b2a6c743375e5a04fee905f68e5bf9a056a69
matejmeglic <matej@matejmeglic.com>
2020-09-02 18:29:47 +0200

    sept-dlabs-update

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,51 +1,63 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class About extends Component {
   render() {
-
-    if(this.props.data){
+    if (this.props.data) {
       var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
+      var profilepic = "images/" + this.props.data.image;
       var bio = this.props.data.bio;
-      var phone= this.props.data.phone;
+      var phone = this.props.data.phone;
       var email = this.props.data.email;
       var skype = this.props.data.skype;
       var resumeDownload = this.props.data.resumedownload;
-
     }
 
     return (
       <section id="about">
-      <div className="row">
-         <div className="three columns">
-            <br /> 
-            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
-         </div>
-         <div className="nine columns main-col">
+        <div className="row">
+          <div className="three columns">
+            <br />
+            <img
+              className="profile-pic"
+              src={profilepic}
+              alt="Matej Meglic Profile Pic"
+            />
+          </div>
+          <div className="nine columns main-col">
             <h2>About Me</h2>
 
             <p>{bio}</p>
             <div className="row">
-               <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-						   <span>{name}</span><br />
-						   <span>{email}</span><br />
-                     <span>{skype}</span><br />
-                     <span>{phone}</span><br />
-                   <br />
-					   </p>
-               </div>
-               <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} className="button" target="_blank" rel="noopener noreferrer"><i className="fa fa-download"></i>Download CV</a>
-                  </p>
-               </div>
+              <div className="columns contact-details">
+                <h2>Contact Details</h2>
+                <p className="address">
+                  <span>{name}</span>
+                  <br />
+                  <span>{email}</span>
+                  <br />
+                  <span>{skype}</span>
+                  <br />
+                  <span>{phone}</span>
+                  <br />
+                  <br />
+                </p>
+              </div>
+              <div className="columns download">
+                <p>
+                  <a
+                    href={resumeDownload}
+                    className="button"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="fa fa-download"></i>Download CV
+                  </a>
+                </p>
+              </div>
             </div>
-         </div>
-      </div>
-
-   </section>
+          </div>
+        </div>
+      </section>
     );
   }
 }
